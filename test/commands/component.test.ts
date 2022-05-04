4d2e49ca60f690d79a586fd7085f21cefb7dddec
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2022-05-05 00:37:08 +0300

    test: more component unit tests

--- test/commands/component.test.ts
+++ test/commands/component.test.ts
@@ -1,7 +1,18 @@
 import {expect, test} from '@oclif/test'
 import * as fs from 'fs-extra'
 
+const TEST_DIR = 'mock/src'
+
 describe('component', () => {
+  before(() => {
+    fs.remove('mock').catch()
+    fs.remove('src/components').catch()
+  })
+  after(() => {
+    fs.remove('mock').catch()
+    fs.remove('src/components').catch()
+  })
+
   test
   .stdout()
   .command(['component'])
@@ -12,15 +23,27 @@ describe('component', () => {
   test
   .stdout()
   .command(['component', 'MyComponent'])
-  .it('should create a component named MyComponent.js in src/components by default', _ctx => {
-    expect(fs.pathExistsSync('src/components/MyComponent/')).to.be.true
-    expect(fs.pathExistsSync('src/components/MyComponent/MyComponent.ts')).to.be.true
+  .it('should create a component named MyComponent.js in src/components by default', async _ctx => {
+    const jsFilePath = 'src/components/MyComponent/MyComponent.js'
+
+    expect(await fs.pathExists(jsFilePath)).to.be.true
+
+    const expectedFileContent = `export default function MyComponent() {
+  return <div>MyComponent</div>
+}`
+    const jsFileContent = await fs.readFile(jsFilePath, 'utf-8')
+
+    expect(jsFileContent).to.eql(expectedFileContent)
   })
 
   test
   .stdout()
-  .command(['component', 'ComponentName', '--dest', 'testSrc/components'])
-  .it('should create a component named ComponentName.js in testSrc/components', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .command(['component', 'TestComponent', '--dest', `${TEST_DIR}/components`])
+  .it('should create a component named TestComponent.js in mock directory', async _ctx => {
+    const parentFolder = await fs.pathExists(TEST_DIR)
+    const targetJsFile = await fs.pathExists(`${TEST_DIR}/components/TestComponent/TestComponent.js`)
+
+    expect(parentFolder).to.be.true
+    expect(targetJsFile).to.be.true
   })
 })
