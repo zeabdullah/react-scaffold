8ec9c62f2d8f65fe7887fdec270f9e497124335d
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2022-05-29 12:39:00 +0300

    test(ComponentTemplate): add more unit tests
    - refactor already included test cases

--- test/templates/ComponentTemplate.test.ts
+++ test/templates/ComponentTemplate.test.ts
@@ -1,12 +1,11 @@
 /* eslint-disable unicorn/filename-case */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {expect, test} from '@oclif/test'
 import ComponentTemplate from '../../src/templates/ComponentTemplate'
 
 describe('ComponentTemplate Class', () => {
-  test
-  .it('should return a CSS+JavaScript template by default', () => {
-    const template =  new ComponentTemplate('ExampleComponent')
-
+  it('should return a CSS+JavaScript template by default', () => {
+    const tp =  new ComponentTemplate('ExampleComponent')
     const expectedJsTemplate =
 `import React from 'react';
 import styles from './ExampleComponent.module.css';
@@ -17,12 +16,30 @@ function ExampleComponent() {
 
 export default ExampleComponent;
 `
-    expect(template.getScriptTemplate()).to.eql(expectedJsTemplate)
+    expect(tp.getScriptTemplate()).to.eql(expectedJsTemplate)
   })
 
-  test
-  .skip()
-  .it('should return', () => {
-    expect.fail()
+  it('should return a template including a SCSS import when set in the config', () => {
+    const tp = new ComponentTemplate('Example', {isScss: true})
+    expect(tp.getScriptTemplate()).to.contain("import styles from './Example.module.scss';")
+    expect(tp.getScriptTemplate()).to.not.contain("import styles from './Example.module.css';")
+  })
+
+  it('should return a TypeScript template when set in the config', () => {
+    const tp =  new ComponentTemplate('Example', {isTypescript: true})
+    const expectedTsTemplate =
+`import React from 'react';
+import styles from './Example.module.css';
+
+interface ExampleProps {}
+
+function Example(): JSX.Element {
+  return <div className={styles.Example}>Example</div>
+}
+
+export default Example;
+`
+
+    expect(tp.getScriptTemplate()).to.eql(expectedTsTemplate)
   })
 })
