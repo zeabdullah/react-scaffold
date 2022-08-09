c9290081974d44b6a94af0fd085b3910bf536b34
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2022-08-09 21:57:00 +0300

    feat: 🎉 implement getSubdirectories helper method function to be used in `rsx init`

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,26 @@
+import {type PathLike} from 'fs-extra'
+import readdirp from 'readdirp'
+
 export function isPascalCase(str: string): boolean {
   const pascalCaseRegex = /^(?:[A-Z][a-z]+)+$/
   return pascalCaseRegex.test(str)
+}
+
+type FilterOptions = {
+  directoryFilter?: string | string[]
+}
+export async function getSubdirectories(
+  dirPath: PathLike,
+  options?: FilterOptions,
+): Promise<string[]> {
+  try {
+    const dirs = await readdirp.promise(dirPath.toString(), {
+      directoryFilter: options?.directoryFilter ?? ['!.*', '!*modules'],
+      type: 'directories',
+    })
+    return dirs.map(dir => dir.path.replace(/\\/g, '/'))
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
--- test/helpers/helpers.test.ts
+++ test/helpers/helpers.test.ts
@@ -1,20 +1,48 @@
 import {expect, test} from '@oclif/test'
-import {isPascalCase} from '../../src/helpers'
+import mock from 'mock-fs'
+import {getSubdirectories, isPascalCase} from '../../src/helpers'
 
 describe('Helper methods', () => {
   describe('isPascalCase', () => {
-    test
-    .it('should return return true if string is in PascalCase', () => {
+    it('should return return true if string is in PascalCase', () => {
       expect(isPascalCase('ImInPascalCase')).to.be.true
       expect(isPascalCase('Mystring')).to.be.true
       expect(isPascalCase('AnotherPascalcasestring')).to.be.true
-    })
 
-    test
-    .it('should return false otherwise', () => {
       expect(isPascalCase('myString')).to.be.false
       expect(isPascalCase('mystring')).to.be.false
       expect(isPascalCase('notPascalCase')).to.be.false
     })
+  })
+
+  describe('getSubdirectories', () => {
+    before(() =>
+      mock({
+        path: {
+          to: {
+            dir1: {
+              file1: 'text 1',
+            },
+            dir2: {
+              file2: 'text 2',
+            },
+          },
+        },
+      }),
+    )
+
+    test.it(
+      'should return the list of subdirectories of a given root folder',
+      async () => {
+        expect(await getSubdirectories(process.cwd())).to.eql([
+          'path',
+          'path/to',
+          'path/to/dir1',
+          'path/to/dir2',
+        ])
+      },
+    )
+
+    after(mock.restore)
   })
 })
